c308955502514d4fd334a4ef43c54bb8d73ce33c
Ahsan <ahsan@psxascend.ai>
2026-03-31 06:14:25 +0500

    Bottom navigation implementation

--- lib/main_scaffold.dart
+++ lib/main_scaffold.dart
@@ -2,14 +2,43 @@ import 'package:flutter/material.dart';
 
 class MainScaffold extends StatefulWidget {
   final Widget body;
+  final int currentIndex;
 
-  MainScaffold({required this.body});
+  MainScaffold({required this.body, this.currentIndex = 0});
 
   @override
   _MainScaffoldState createState() => _MainScaffoldState();
 }
 
 class _MainScaffoldState extends State<MainScaffold> {
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _selectedIndex = widget.currentIndex;
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, '/');
+        break;
+      case 1:
+        Navigator.pushNamed(context, '/row');
+        break;
+      case 2:
+        Navigator.pushNamed(context, '/column');
+        break;
+      case 3:
+        Navigator.pushNamed(context, '/profile_form');
+        break;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +56,20 @@ class _MainScaffoldState extends State<MainScaffold> {
         ),
       ),
       body: widget.body,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+            //backgroundColor: Colors.yellow,
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.view_week), label: 'Rows'),
+          BottomNavigationBarItem(icon: Icon(Icons.view_column), label: 'Columns')
+        ],
+      ),
     );
   }
 }
